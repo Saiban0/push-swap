8418bfd2d675c55c6f93a1c44ea875b0c76cd662
Saiban0 <bartched@outlook.com>
2024-02-08 17:05:39 +0100

    on va regarder pipex

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:38:58 by bchedru           #+#    #+#             */
-/*   Updated: 2024/02/06 19:22:29 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/02/08 17:01:13 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,40 @@ int	main(void)
 	t_list	*youpi;
 	t_list	*tralala;
 	t_list	*tjhk;
+	t_list	*qwe;
+	t_list	*rty;
+	t_list	*uio;
+	t_list	*b;
 
 	youpi = ft_lstnew(21);
 	tralala = ft_lstnew(42);
 	tjhk = ft_lstnew(84);
+	qwe = ft_lstnew(69);
+	rty = ft_lstnew(420);
+	uio = ft_lstnew(2);
+	b = NULL;
 	ft_lstadd_front(&youpi, tralala);
 	ft_lstadd_front(&youpi, tjhk);
-	ft_reverse_rotate(&tralala, 'a');
-	ft_printf("%d", youpi->next->next->content);
+	ft_lstadd_front(&youpi, qwe);
+	ft_lstadd_front(&youpi, rty);
+	ft_lstadd_front(&youpi, uio);
+	ft_push(&youpi, &b, 'b');
+	while(youpi)
+	{
+		if(push_where(&b, youpi->content))
+		{
+			ft_push(&youpi, &b, 'b');
+			ft_rotate(&b, 'b');
+		}
+		else
+			ft_push(&youpi, &b, 'b');
+	}
+	while(b)
+		ft_push(&b, &youpi, 'a');
+	while(youpi)
+	{
+		ft_printf("%d\n",youpi->content);
+		youpi = youpi->next;
+	}
 	return (0);
 }
--- turksort_utils.c
+++ turksort_utils.c
@@ -6,13 +6,13 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 18:22:51 by bchedru           #+#    #+#             */
-/*   Updated: 2024/02/06 19:18:53 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/02/08 15:09:57 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	push_bottom(t_list **stack, int data)
+int	push_where(t_list **stack, int data)
 {
 	return (((*stack)->content) > data);
 }
