b83791d79d5ad1726c74624c0a67463ba3ba555e
Saiban0 <bartched@outlook.fr>
2024-03-13 15:43:27 +0100

    Deplacement stack_init

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 17:23:23 by bchedru           #+#    #+#             */
-/*   Updated: 2024/03/12 18:00:31 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/03/13 15:43:11 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,34 +46,22 @@ void	set_price(t_list *a, t_list *b)
 		if (b->target->above_median)
 			b->cost += b->target->curr_pos;
 		else
-			b->cost + len_a - (b->target->curr_pos);
+			b->cost += len_a - (b->target->curr_pos);
 		b = b->next;
 	}
 }
 
-void	stack_init(t_list **a, char **argv, int flag_free_heap)
+void	set_cheapest(t_list *stack)
 {
-	long	nbr;
-	int		i;
-	t_list	*temp;
+	int	min;
 
-	i = 0;
-	temp = NULL;
-	while (argv[i])
+	min = stack->cost;
+	while (stack)
 	{
-		if (error_syntax(argv[i]))
-			error_free(a, argv, flag_free_heap);
-		nbr = ft_atol(argv[i]);
-		if (nbr > INT_MAX || nbr < INT_MIN)
-			error_free(a, argv, flag_free_heap);
-		if (error_repetition(*a, (int)nbr))
-			error_free(a, argv, flag_free_heap);
-		temp = ft_lstnew((int) nbr);
-		ft_lstadd_back(a, temp);
-		i++;
+		if (stack->cost < min)
+			min = stack->cost;
+		stack = stack->next;
 	}
-	if (flag_free_heap)
-		free_matrix(argv);
 }
 
 void init_nodes (t_list *a, t_list *b)
--- main.c
+++ main.c
@@ -6,19 +6,44 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:58:10 by bchedru           #+#    #+#             */
-/*   Updated: 2024/03/12 16:53:48 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/03/13 15:43:09 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// void	push_swap(t_list *a, t_list *b)
-// {
-// 	if (ft_lstsize(a) == 5)
-// 	{
-		
-// 	}
-// }
+void	push_swap(t_list **a, t_list **b)
+{
+	while(ft_lstsize(*a) > 3)
+		ft_push(a, b, 'b');
+	tiny_sort(a);
+	
+}
+
+void	stack_init(t_list **a, char **argv, int flag_free_heap)
+{
+	long	nbr;
+	int		i;
+	t_list	*temp;
+
+	i = 0;
+	temp = NULL;
+	while (argv[i])
+	{
+		if (error_syntax(argv[i]))
+			error_free(a, argv, flag_free_heap);
+		nbr = ft_atol(argv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			error_free(a, argv, flag_free_heap);
+		if (error_repetition(*a, (int)nbr))
+			error_free(a, argv, flag_free_heap);
+		temp = ft_lstnew((int) nbr);
+		ft_lstadd_back(a, temp);
+		i++;
+	}
+	if (flag_free_heap)
+		free_matrix(argv);
+}
 
 int	main(int argc, char **argv)
 {
