ce489487a85a2117a7ab14984025ddbcba36e568
Saiban0 <Saiban0@github.com>
2024-03-21 16:20:11 +0100

    push_swap reorganisation

--- main.c
+++ main.c
@@ -6,34 +6,23 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:58:10 by bchedru           #+#    #+#             */
-/*   Updated: 2024/03/20 17:07:34 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/03/21 15:40:39 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_swap(t_list **a, t_list **b)
+void	tiny_sort(t_list **stack)
 {
-	int		len_a;
-	t_list	*lowest;
+	t_list	*highest;
 
-	len_a = ft_lstsize(*a);
-	while (len_a-- > 3)
-		pb(b, a, 0);
-	tiny_sort(a);
-	while (*b)
-	{
-		init_nodes(*a, *b);
-		//move_nodes(a, b);
-	}
-	set_current_pos(*a);
-	lowest = find_lowest(*a);
-	if (lowest->above_median)
-		while (*a != lowest)
-			ra(a, 0);
-	else
-		while (*a != lowest)
-			rra(a, 0);
+	highest = find_highest(*stack);
+	if (*stack == highest)
+		ra(stack, 0);
+	else if ((*stack)->next == highest)
+		rra(stack, 0);
+	if ((*stack)->content > (*stack)->next->content)
+		sa(stack, 0);
 }
 
 int	main(int argc, char **argv)
---
+++ push_swap.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 12:44:41 by bchedru           #+#    #+#             */
+/*   Updated: 2024/03/21 16:17:17 by bchedru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	rr_to_cheapest(t_list **a, t_list **b, t_list *cheapest)
+{
+	while (*a != cheapest->target && *b != cheapest)
+		rr(a, b, 0);
+	set_current_pos(*a);
+	set_current_pos(*b);
+}
+
+static void	rrr_to_cheapest(t_list **a, t_list **b, t_list *cheapest)
+{
+	while (*a != cheapest->target && *b != cheapest)
+		rrr(a, b, 0);
+	set_current_pos(*a);
+	set_current_pos(*b);
+}
+
+void	finish_rotation(t_list **stack, t_list *top, char name)
+{
+	while (*stack != top)
+	{
+		if (name == 'a')
+		{
+			if (top->above_median)
+				ra(stack, 0);
+			else
+				rra(stack, 0);
+		}
+		else if (name == 'b')
+		{
+			if (top->above_median)
+				rb(stack, 0);
+			else
+				rrb(stack, 0);
+		}
+	}
+}
+
+static void	process_stacks(t_list **a, t_list **b)
+{
+	t_list	*cheapest;
+
+	cheapest = find_cheapest(*b);
+	if (cheapest->above_median
+		&& cheapest->target->above_median)
+		rr_to_cheapest(a, b, cheapest);
+	else if (!(cheapest->above_median)
+		&& !(cheapest->target->above_median))
+		rrr_to_cheapest(a, b, cheapest);
+	finish_rotation(b, cheapest, 'b');
+	finish_rotation(a, cheapest, 'a');
+	pa(a, b, 0);
+}
+
+void	push_swap(t_list **a, t_list **b)
+{
+	int		len_a;
+	t_list	*lowest;
+
+	len_a = ft_lstsize(*a);
+	while (len_a-- > 3)
+		pb(b, a, 0);
+	tiny_sort(a);
+	while (*b)
+	{
+		init_nodes(*a, *b);
+		process_stacks(a, b);
+	}
+	set_current_pos(*a);
+	lowest = find_cheapest(*a);
+	if (lowest->above_median)
+		while (*a != lowest)
+			ra(a, 0);
+	else
+		while (*a != lowest)
+			rra(a, 0);
+}
